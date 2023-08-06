a38bb58b70c8c00437a8d81c4167f3cac040c216
Solivagant14 <nishantjha360@gmail.com>
2023-08-06 13:21:26 +0530

    change host and port

--- api.py
+++ api.py
@@ -13,4 +13,4 @@ def generate(digit=False,uppercase=False,schar=True,length=12):
     return {'response': generate_password(digit,uppercase,schar,length)}
 
 if __name__=='__main__':
-    uvicorn.run(app,host='127.0,0,1',port=8000)
+    uvicorn.run(app,host='0.0.0.0',port=10000)
