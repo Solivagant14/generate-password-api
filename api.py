9d2d6bee539a401d60bec99df4e3ad6603562d34
Solivagant14 <nishantjha360@gmail.com>
2023-08-09 16:50:53 +0530

    add cors

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from main import generate_password
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def index():
